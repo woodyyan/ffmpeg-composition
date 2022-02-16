3c8e65c3e48a1003ba0590212a3be59861d2263f
Woody <colorguitar@hotmail.com>
2022-02-16 11:40:39 +0800

    support new line

--- index.py
+++ index.py
@@ -158,7 +158,10 @@ def calc_pic_param(pictures, input_path, output_path):
 def calc_text_param(texts):
     text_param = ','
     template = "drawtext=fontfile=/tmp/%s:text='%s':fontcolor=white:fontsize=%d:box=%s:boxcolor=black@0.24:boxborderw=5:x=%s:y=%s:fix_bounds=true,"
+    max_len = 22  # 22个中文字符会超出屏幕
     for text in texts:
+        if len(text.content) > max_len:
+            text.content = text.content[:max_len] + '\n' + text.content[max_len:]
         text_param += template % (font_file, text.content, text.size, text.enable_box, text.x, text.y)
 
     return text_param[0:len(text_param) - 1]
